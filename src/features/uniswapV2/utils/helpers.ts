02fb0848814c713fe7c575b9dc1762127edadf14
Alberto <25537372+albertobas@users.noreply.github.com>
2023-04-03 15:32:43 +0200

    fix: avoid listing pools and tokens without volume

--- src/features/uniswapV2/utils/helpers.ts
+++ src/features/uniswapV2/utils/helpers.ts
@@ -10,44 +10,46 @@ export const getFormattedPairsUniswapV2 = (pairs: Pools, networkId: string): Poo
   const poolsStateData: PoolsStateData = {};
 
   for (const address of Object.keys(pairs.current)) {
-    const [volume, volumeChange] =
-      pairs.t1D[address] && pairs.t2D[address]
-        ? get2DayChange(pairs.current[address].volume, pairs.t1D[address].volume, pairs.t2D[address].volume)
-        : pairs.current[address].volume
-        ? [pairs.current[address].volume, 0]
-        : [0, 0];
-    const fees = (volume * pairs.current[address].feeTier) / 1000000;
-    const volume1W = pairs.t1W[address]
-      ? pairs.current[address].volume - pairs.t1W[address].volume
-      : pairs.current[address].volume;
-    const tvlChange = pairs.t1D[address]
-      ? ((pairs.current[address].tvl - pairs.t1D[address].tvl) /
-          (pairs.t1D[address].tvl === 0 ? 1 : pairs.t1D[address].tvl)) *
-        100
-      : 0;
-    poolsObject[address] = {
-      address: pairs.current[address].address,
-      feeTier: pairs.current[address].feeTier,
-      volume,
-      volumeChange,
-      volume1W,
-      token0: {
-        name: pairs.current[address].token0.name,
-        symbol: pairs.current[address].token0.symbol,
-        address: pairs.current[address].token0.address,
-        decimals: pairs.current[address].token0.decimals,
-      },
-      token1: {
-        name: pairs.current[address].token1.name,
-        symbol: pairs.current[address].token1.symbol,
-        address: pairs.current[address].token1.address,
-        decimals: pairs.current[address].token1.decimals,
-      },
-      tvl: pairs.current[address].tvl,
-      tvlChange,
+    if (pairs.current[address].volume > 0) {
+      const [volume, volumeChange] =
+        pairs.t1D[address] && pairs.t2D[address]
+          ? get2DayChange(pairs.current[address].volume, pairs.t1D[address].volume, pairs.t2D[address].volume)
+          : pairs.current[address].volume
+          ? [pairs.current[address].volume, 0]
+          : [0, 0];
+      const fees = (volume * pairs.current[address].feeTier) / 1000000;
+      const volume1W = pairs.t1W[address]
+        ? pairs.current[address].volume - pairs.t1W[address].volume
+        : pairs.current[address].volume;
+      const tvlChange = pairs.t1D[address]
+        ? ((pairs.current[address].tvl - pairs.t1D[address].tvl) /
+            (pairs.t1D[address].tvl === 0 ? 1 : pairs.t1D[address].tvl)) *
+          100
+        : 0;
+      poolsObject[address] = {
+        address: pairs.current[address].address,
+        feeTier: pairs.current[address].feeTier,
+        volume,
+        volumeChange,
+        volume1W,
+        token0: {
+          name: pairs.current[address].token0.name,
+          symbol: pairs.current[address].token0.symbol,
+          address: pairs.current[address].token0.address,
+          decimals: pairs.current[address].token0.decimals,
+        },
+        token1: {
+          name: pairs.current[address].token1.name,
+          symbol: pairs.current[address].token1.symbol,
+          address: pairs.current[address].token1.address,
+          decimals: pairs.current[address].token1.decimals,
+        },
+        tvl: pairs.current[address].tvl,
+        tvlChange,
 
-      fees,
-    };
+        fees,
+      };
+    }
   }
   poolsStateData[networkId] = {
     pools: poolsObject,
@@ -75,26 +77,28 @@ export const getFormattedTokensUniswapV2 = (
         ? [volumeCurrent, null]
         : [null, null];
     const price = ethers.current ? tokens.current[address].derivedETH * ethers.current : 0;
-    tokensObject[address] = {
-      name: tokens.current[address].name,
-      symbol: tokens.current[address].symbol,
-      address: tokens.current[address].address,
-      derivedETH: tokens.current[address].derivedETH,
-      volume: volume,
-      volumeChange: volumeChange,
-      volume1W: tokens.t1W[address] && volumeCurrent && volumeT1W ? volumeCurrent - volumeT1W : volumeCurrent,
-      tvl: tokens.current[address].tvl * ethers.current,
-      tvlChange: tokens.t1D[address]
-        ? getPercentChange(tokens.current[address].tvl * ethers.current, tokens.t1D[address].tvl * ethers.t1D)
-        : null,
-      price: price,
-      priceChange: tokens.t1D[address]
-        ? getPercentChange(price, ethers.t1D ? tokens.t1D[address].derivedETH * ethers.t1D : 0)
-        : null,
-      // priceChange1W is not currently used in the user token table. However, the idea is to keep it in case a new
-      // component is implemented to show individual token stats
-      priceChange1W: tokens.t1W[address] && ethers.t1W ? tokens.t1W[address].derivedETH * ethers.t1W : null,
-    };
+    if (volume) {
+      tokensObject[address] = {
+        name: tokens.current[address].name,
+        symbol: tokens.current[address].symbol,
+        address: tokens.current[address].address,
+        derivedETH: tokens.current[address].derivedETH,
+        volume: volume,
+        volumeChange: volumeChange,
+        volume1W: tokens.t1W[address] && volumeCurrent && volumeT1W ? volumeCurrent - volumeT1W : volumeCurrent,
+        tvl: tokens.current[address].tvl * ethers.current,
+        tvlChange: tokens.t1D[address]
+          ? getPercentChange(tokens.current[address].tvl * ethers.current, tokens.t1D[address].tvl * ethers.t1D)
+          : null,
+        price: price,
+        priceChange: tokens.t1D[address]
+          ? getPercentChange(price, ethers.t1D ? tokens.t1D[address].derivedETH * ethers.t1D : 0)
+          : null,
+        // priceChange1W is not currently used in the user token table. However, the idea is to keep it in case a new
+        // component is implemented to show individual token stats
+        priceChange1W: tokens.t1W[address] && ethers.t1W ? tokens.t1W[address].derivedETH * ethers.t1W : null,
+      };
+    }
   }
   tokensStateData[networkId] = {
     tokens: tokensObject,
--- src/features/uniswapV3/utils/helpers.ts
+++ src/features/uniswapV3/utils/helpers.ts
@@ -10,43 +10,45 @@ export const getFormattedPoolsUniswapV3 = (pools: Pools, networkId: string): Poo
   const poolsStateData: PoolsStateData = {};
 
   for (const address of Object.keys(pools.current)) {
-    const [volume, volumeChange] =
-      pools.t1D[address] && pools.t2D[address]
-        ? get2DayChange(pools.current[address].volume, pools.t1D[address].volume, pools.t2D[address].volume)
-        : pools.current[address].volume
-        ? [pools.current[address].volume, 0]
-        : [0, 0];
-    const volume1W = pools.t1W[address]
-      ? pools.current[address].volume - pools.t1W[address].volume
-      : pools.current[address].volume;
-    const tvlChange = pools.t1D[address]
-      ? ((pools.current[address].tvl - pools.t1D[address].tvl) /
-          (pools.t1D[address].tvl === 0 ? 1 : pools.t1D[address].tvl)) *
-        100
-      : 0;
-    const fees = (volume * pools.current[address].feeTier) / 1000000;
-    poolsObject[address] = {
-      address: pools.current[address].address,
-      feeTier: pools.current[address].feeTier,
-      fees,
-      volume,
-      volumeChange,
-      volume1W,
-      tvl: pools.current[address].tvl,
-      tvlChange,
-      token0: {
-        name: pools.current[address].token0.name,
-        symbol: pools.current[address].token0.symbol,
-        address: pools.current[address].token0.address,
-        decimals: pools.current[address].token0.decimals,
-      },
-      token1: {
-        name: pools.current[address].token1.name,
-        symbol: pools.current[address].token1.symbol,
-        address: pools.current[address].token1.address,
-        decimals: pools.current[address].token1.decimals,
-      },
-    };
+    if (pools.current[address].volume > 0) {
+      const [volume, volumeChange] =
+        pools.t1D[address] && pools.t2D[address]
+          ? get2DayChange(pools.current[address].volume, pools.t1D[address].volume, pools.t2D[address].volume)
+          : pools.current[address].volume
+          ? [pools.current[address].volume, 0]
+          : [0, 0];
+      const volume1W = pools.t1W[address]
+        ? pools.current[address].volume - pools.t1W[address].volume
+        : pools.current[address].volume;
+      const tvlChange = pools.t1D[address]
+        ? ((pools.current[address].tvl - pools.t1D[address].tvl) /
+            (pools.t1D[address].tvl === 0 ? 1 : pools.t1D[address].tvl)) *
+          100
+        : 0;
+      const fees = (volume * pools.current[address].feeTier) / 1000000;
+      poolsObject[address] = {
+        address: pools.current[address].address,
+        feeTier: pools.current[address].feeTier,
+        fees,
+        volume,
+        volumeChange,
+        volume1W,
+        tvl: pools.current[address].tvl,
+        tvlChange,
+        token0: {
+          name: pools.current[address].token0.name,
+          symbol: pools.current[address].token0.symbol,
+          address: pools.current[address].token0.address,
+          decimals: pools.current[address].token0.decimals,
+        },
+        token1: {
+          name: pools.current[address].token1.name,
+          symbol: pools.current[address].token1.symbol,
+          address: pools.current[address].token1.address,
+          decimals: pools.current[address].token1.decimals,
+        },
+      };
+    }
   }
   poolsStateData[networkId] = {
     pools: poolsObject,
@@ -74,24 +76,26 @@ export const getFormattedTokensUniswapV3 = (
         ? [volumeCurrent, null]
         : [null, null];
     const price = etherPrices.current ? tokens.current[address].derivedETH * etherPrices.current : 0;
-    tokensObject[address] = {
-      name: tokens.current[address].name,
-      symbol: tokens.current[address].symbol,
-      address: tokens.current[address].address,
-      derivedETH: tokens.current[address].derivedETH,
-      volume: volume,
-      volumeChange: volumeChange,
-      volume1W: tokens.t1W[address] && volumeCurrent && volumeT1W ? volumeCurrent - volumeT1W : volumeCurrent,
-      tvl: tokens.current[address].tvl,
-      tvlChange: tokens.t1D[address] ? getPercentChange(tokens.current[address].tvl, tokens.t1D[address].tvl) : null,
-      price: price,
-      priceChange: tokens.t1D[address]
-        ? getPercentChange(price, etherPrices.t1D ? tokens.t1D[address].derivedETH * etherPrices.t1D : 0)
-        : null,
-      // priceChange1W is not currently used in the user token table. However, the idea is to keep it in case a new
-      // component is implemented to show individual token stats
-      priceChange1W: tokens.t1W[address] && etherPrices.t1W ? tokens.t1W[address].derivedETH * etherPrices.t1W : null,
-    };
+    if (volume) {
+      tokensObject[address] = {
+        name: tokens.current[address].name,
+        symbol: tokens.current[address].symbol,
+        address: tokens.current[address].address,
+        derivedETH: tokens.current[address].derivedETH,
+        volume: volume,
+        volumeChange: volumeChange,
+        volume1W: tokens.t1W[address] && volumeCurrent && volumeT1W ? volumeCurrent - volumeT1W : volumeCurrent,
+        tvl: tokens.current[address].tvl,
+        tvlChange: tokens.t1D[address] ? getPercentChange(tokens.current[address].tvl, tokens.t1D[address].tvl) : null,
+        price: price,
+        priceChange: tokens.t1D[address]
+          ? getPercentChange(price, etherPrices.t1D ? tokens.t1D[address].derivedETH * etherPrices.t1D : 0)
+          : null,
+        // priceChange1W is not currently used in the user token table. However, the idea is to keep it in case a new
+        // component is implemented to show individual token stats
+        priceChange1W: tokens.t1W[address] && etherPrices.t1W ? tokens.t1W[address].derivedETH * etherPrices.t1W : null,
+      };
+    }
   }
   tokensStateData[networkId] = {
     tokens: tokensObject,
